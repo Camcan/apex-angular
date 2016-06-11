478b495cf886b4ffe1f60abe99cdc72536ebcd65
Camcan <mr.campbell.hawkes@gmail.com>
2016-06-11 18:44:11 +1200

    Remove Slider & related Scripts from Homepage

--- js/controllers.js
+++ js/controllers.js
@@ -50,43 +50,7 @@ apex.controller('mainController', function($scope, $location, ImageDatabase) {
 })
 
 apex.controller('homeController', function($scope, $timeout) {
-	var i = 0
-	$scope.slideOut = false
-	var sections = (function(){
-			var keys = []
-			for (var k in $scope.art) keys.push(k)
-			return keys
-		})()
-	var currentSection = sections[0]
 	
-	var iterateThroughImages = function(){
-		var toShow = []
-		for (var j = 0; j < 4; j+=1){
-			if (i < $scope.art[currentSection].length){
-				toShow.push($scope.art[currentSection][i]) 
-			} else {
-				i = 0 
-				var sectionIndex = sections.indexOf(currentSection) + 1
-				if (sectionIndex == 4) {
-					currentSection = sections[0]
-				} else {
-					currentSection = sections[(sectionIndex)]
-				}
-				
-				toShow.push($scope.art[currentSection][i])
-			}
-			i += 1
-
-			if (!$scope.isActive("/")) return	
-		}
-		$scope.slideOut = true
-		$scope.slides = toShow
-		console.log($scope.slideOut)
-		$timeout(iterateThroughImages, 5000)
-		$scope.slideOut = false
-	}
-    iterateThroughImages()
-    
 })
 
 apex.controller('artController', function($scope, $location, PortfolioSelection) {
